42fadc50c5b721fe27bbdab5d3724b5a368b859a
Matthew-Tolentino <50345615+Matthew-Tolentino@users.noreply.github.com>
2019-05-14 23:51:32 -0700

    Update main.js

--- greenhouse_granny/js/main.js
+++ greenhouse_granny/js/main.js
@@ -35,9 +35,7 @@ MainMenu.prototype = {
 	}
 }
 
-var Play = function(game){
-	this.ALPHA = 0.0;
-};
+var Play = function(game){};
 Play.prototype = {
 	preload: function(){
 		game.load.image('granny', 'assets/img/Gardener.png'); // replace with spritesheet/texture atlas
@@ -49,7 +47,7 @@ Play.prototype = {
 		game.load.image('spitter plant', 'assets/img/Spitter_Plant.png');
 
 		//Load in tilemap and spritesheet
-		game.load.tilemap('level', 'assets/tilemaps/tempTileMap.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.tilemap('level', 'assets/tilemaps/Level1Tilemap.json', null, Phaser.Tilemap.TILED_JSON);
 		game.load.spritesheet('tilesheet', 'assets/img/Tilesheet.png', 64, 64);
 
 		game.load.audio('track01', 'assets/audio/Track 1.ogg');
@@ -70,7 +68,8 @@ Play.prototype = {
 
 		//Tilemap creation
 		this.map = game.add.tilemap('level');
-		this.map.addTilesetImage('TempTileSet', 'tilesheet');
+		this.map.addTilesetImage('tempTileset', 'tilesheet');
+		this.map.setCollisionByExclusion([]);
 		this.mapLayer = this.map.createLayer('Tile Layer 1');
 		this.mapLayer.resizeWorld();
 
@@ -78,7 +77,7 @@ Play.prototype = {
 		this.enemies = game.add.group();
 
 		// Set up the player
-		this.player = new Granny(game, 100, 400, this.enemies);
+		this.player = new Granny(game, 90, 1800, this.enemies);
 		this.player.switchWeapon(shovel);
 		game.add.existing(this.player);
 		game.camera.follow(this.player);
@@ -88,97 +87,12 @@ Play.prototype = {
     	this.healthBar.fixedToCamera = true;
     	this.healthBar.cameraOffset.setTo(16, 16);
 
-		// A group that holds all the platforms. It's a wonky workaround for now.
-		this.platforms = game.add.group();
-		this.platforms.enableBody = true;
-
-		//Collison platforms for tilemap *Temp solution*
-		var ground = this.platforms.create(85, 730, 'platform');
-		ground.anchor.set(.5, .5);
-		ground.scale.x = 2.75;
-
-		//Side wall falling from starting platform
-		ground = this.platforms.create(640, 780, 'platform');
-		ground.scale.y = 50;
-		ground.scale.x = .15;
-		//ground.angle += 90;
-
-		//ground below starting platform
-		ground = this.platforms.create(500, 1860, 'platform');
-
-		//Stairs to go down to bottom
-		ground = this.platforms.create(900, 1935, 'platform');
-		ground.anchor.set(.5);
-		ground.scale.x = .25;
-
-		ground = this.platforms.create(950, 1975, 'platform');
-		ground.anchor.set(.5);
-		ground.scale.x = .25;
-
-		//Bottom
-		ground = this.platforms.create(900, 1985, 'platform');
-		ground.scale.x = 10;
-
-		//stairs to go up hill
-		ground = this.platforms.create(2535, 1955, 'platform');
-		ground.scale.x = 3.5;
-
-		ground = this.platforms.create(2575, 1920, 'platform');
-		ground.scale.x = 3.3;
-
-		ground = this.platforms.create(2615, 1870, 'platform');
-		ground.scale.x = 3.1;
-
-		ground = this.platforms.create(2650, 1830, 'platform');
-		ground.scale.x = 2.9;
-
-		ground = this.platforms.create(2685, 1790, 'platform');
-		ground.scale.x = 2.75;
-
-		ground = this.platforms.create(2735, 1745, 'platform');
-		ground.scale.x = 2.5;
-
-		ground = this.platforms.create(2535, 1955, 'platform');
-		ground.scale.x = 2.3;
-
-		//top of hill
-		ground = this.platforms.create(2755, 1730, 'platform');
-		ground.scale.x = 2.4;
-
-		//wall after hill
-		ground = this.platforms.create(4610, 1200, 'platform');
-		ground.anchor.x = 0;
-		ground.scale.y = 16;
-		ground.scale.x = .25;
-
-		//Floating this.platforms
-		ground = this.platforms.create(1027, 325, 'platform'); 
-		ground.scale.y = 1;
-		ground.scale.x = 1.1;
-
-		ground = this.platforms.create(1730, 645, 'platform');
-		ground.scale.y = 1;
-		ground.scale.x = 1.1;	
-
-		ground = this.platforms.create(2436, 325, 'platform');
-		ground.scale.y = 1;
-		ground.scale.x = 1.1;	
-
-		ground = this.platforms.create(3140, 645, 'platform');
-		ground.scale.y = 1;
-		ground.scale.x = 1.1;
-
-		this.platforms.forEach(function(platform){
-			platform.body.immovable = true;
-			platform.alpha = this.ALPHA;		
-		}, this, true);
-
 		// A group that holds all the enemy projectiles
 		this.enemyProjectiles = game.add.group();
 		this.enemyProjectiles.enableBody = true;
 
 		// Set up the enemies
-		this.enemies.add(new Enemy(game, 475, 550, this.player, this.enemyProjectiles));
+		this.enemies.add(new Enemy(game, 500, 1700, this.player, this.enemyProjectiles));
     	
 	},
 
@@ -188,8 +102,8 @@ Play.prototype = {
 		// Keep in mind that collide repels the objects, while overlap does not
 
 		// Terrain collisions
-		game.physics.arcade.collide(this.player, this.platforms);
-		game.physics.arcade.collide(this.enemies, this.platforms);
+		game.physics.arcade.collide(this.player, this.mapLayer);
+		game.physics.arcade.collide(this.enemies, this.mapLayer);
 		// probably check for enemyProjectiles too, but we can implement that later on
 
 		// The various collisions which cause the player to take damage
