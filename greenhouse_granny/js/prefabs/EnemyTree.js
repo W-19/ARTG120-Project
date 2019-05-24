6960dd6270c2c870cff7d6c86b57ca5f482fa22c
Jack Cuneo <jcuneo@ucsc.edu>
2019-05-23 18:11:03 -0700

    added enemy moving animation

--- greenhouse_granny/js/prefabs/EnemyTree.js
+++ greenhouse_granny/js/prefabs/EnemyTree.js
@@ -35,6 +35,7 @@ EnemyTree.prototype.update = function() {
 	if(this.burstCooldown > 0) this.burstCooldown--;
 	if(this.acornCooldown > 0) this.acornCooldown--;
 	if(this.hitStunDuration > 0) this.hitStunDuration--;
+
 	//Attacking
 	if(this.hitStunDuration == 0){
 		// Checking to see if player is in range of plant to be shot at, and handling plant movement in this scenario
@@ -51,7 +52,6 @@ EnemyTree.prototype.update = function() {
 				EnemyTree.acorns.add(new EnemyJumper(game, this.x, this.y, this.player, 545, 1050));
 				this.acornCooldown = 500;
 			}
-			console.log("here");
 			if (this.burstShooting == true) {
 				if (this.burstCooldown % 11 == 0) {	
 					bullet = this.enemyProjectiles.create(this.x + (this.facing == 'left' ? -42 : 42), this.y-40, 'seed projectile');
